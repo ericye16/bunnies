29333d36f64e2a52301e4be46cbc561e024bb8a1
Eric Ye <me@ericye16.com>
2015-07-22 22:58:41 -0400

    more URLs

--- bunnies.py
+++ bunnies.py
@@ -4,6 +4,11 @@ import webbrowser, random
 URLS = [
         "https://i.imgur.com/TgyTYlX.gifv",
         "https://i.imgur.com/zwK7XG6.gifv",
+        "https://i.imgur.com/gumTewz.gifv",
+        "http://i.imgur.com/XTuRoOs.gif",
+        "https://gfycat.com/PessimisticBrilliantGroundhog",
+        "https://i.imgur.com/stivcuE.jpg",
+        "http://i.imgur.com/MKhq0Zc.gifv",
         ]
 
 if __name__ == "__main__":
