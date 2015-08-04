173d75592e4ae90df38979569fb07370ca1c47b8
Eric Ye <me@ericye16.com>
2015-08-04 15:51:59 -0400

    moar bunnies

--- bunnies.py
+++ bunnies.py
@@ -9,6 +9,8 @@ URLS = [
         "https://gfycat.com/PessimisticBrilliantGroundhog",
         "https://i.imgur.com/stivcuE.jpg",
         "http://i.imgur.com/MKhq0Zc.gifv",
+        "https://i.imgur.com/8z4Zno4.jpg",
+        "https://instagram.com/p/54ykOHzD60/",
         ]
 
 if __name__ == "__main__":
